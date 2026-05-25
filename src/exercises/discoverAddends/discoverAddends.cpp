5c1250da2bf22d6f862a7be109032e60b80d7e23
Gabriel Malosto <professional@gabdumal.com>
2026-05-25 04:20:31 -0300

    Implement exercise to discover addends of an addition among sorted options

--- src/exercises/discoverAddends/discoverAddends.cpp
+++ src/exercises/discoverAddends/discoverAddends.cpp
@@ -1,11 +1,32 @@
 #include "discoverAddends.hpp"
 
+#include <expected>
 #include <print>
 
-DiscoverAddends::Addends
+std::expected<DiscoverAddends::Addends, std::string>
     DiscoverAddends::discoverAddendsWithSortedOptions(
         std::vector<int> options, int sum) {
-    return {0, 0};
+    unsigned int index_of_first_addend = 0;
+    unsigned int index_of_second_addend = ((unsigned int) options.size()) - 1;
+
+    while (index_of_first_addend < index_of_second_addend) {
+        auto result =
+            options[index_of_first_addend] +
+            options[index_of_second_addend];
+
+        if (result == sum) {
+            return {{options[index_of_first_addend],
+                     options[index_of_second_addend]}};
+        }
+
+        if (result < sum) {
+            index_of_first_addend++;
+        } else {
+            index_of_second_addend--;
+        }
+    }
+
+    return std::unexpected(message_for_not_found_addends);
 }
 
 void DiscoverAddends::run(
@@ -13,17 +34,28 @@ void DiscoverAddends::run(
     std::println("Options: {}", options);
     std::println("Sum: {}", sum);
 
-    auto addends =
+    auto result =
         is_sorted
             ? DiscoverAddends::discoverAddendsWithSortedOptions(options, sum)
             : DiscoverAddends::discoverAddendsWithSortedOptions(options, sum);
 
+    if (!result) {
+        std::println("Error: {}", result.error());
+    }
+
+    auto addends = result.value();
     std::println("Addends: ({}, {})", addends.first, addends.second);
+    std::println();
 }
 
 void DiscoverAddends::main() {
-    std::vector options = {3, 7, 5, 4};
-    int sum = 8;
+    DiscoverAddends::run(
+        {3, 4, 5, 7},
+        8,
+        true);
 
-    DiscoverAddends::run(options, sum, true);
+    DiscoverAddends::run(
+        {3, 4, 4, 7},
+        8,
+        true);
 }
--- src/exercises/discoverAddends/discoverAddends.hpp
+++ src/exercises/discoverAddends/discoverAddends.hpp
@@ -1,6 +1,8 @@
 #ifndef __DISCOVER_ADDENDS_HPP__
 #define __DISCOVER_ADDENDS_HPP__
 
+#include <expected>
+#include <string>
 #include <vector>
 
 class DiscoverAddends {
@@ -11,8 +13,9 @@ class DiscoverAddends {
         };
 
     private:
-        static Addends discoverAddendsWithSortedOptions(
-            std::vector<int> options, int sum);
+        static constexpr auto message_for_not_found_addends = "Addends could not be found.";
+
+        static std::expected<Addends, std::string> discoverAddendsWithSortedOptions(std::vector<int> options, int sum);
 
         static void run(
             std::vector<int> options, int sum, bool is_sorted);
